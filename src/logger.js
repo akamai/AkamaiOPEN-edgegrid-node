ed8149ceb63f31a141203f5c2598060ba2f546e8
Paul Smith <paul@paulsmith.io>
2018-03-27 14:24:32 -0400

    Update log4js dependency

--- src/logger.js
+++ src/logger.js
@@ -15,8 +15,6 @@
 var log4js = require('log4js'),
     logger = log4js.getLogger();
 
-if (!process.env.LOG4JS_CONFIG) {
-  logger.setLevel(log4js.levels.ERROR);
-}
+logger.level = process.env.LOG4JS_CONFIG || 'error';
 
 module.exports = logger;
