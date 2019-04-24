7668512665d8b08849b80cba14f2ad64ccf42944
Tristan Annequin <tannequi@akamai.com>
2019-04-24 14:51:08 +0200

    Add jsesc library in order to escape unicode characters from request body

--- src/api.js
+++ src/api.js
@@ -14,6 +14,7 @@
 
 var request = require('request'),
   url = require('url'),
+  jsesc = require('jsesc'),
   auth = require('./auth'),
   edgerc = require('./edgerc'),
   helpers = require('./helpers'),
@@ -62,6 +63,7 @@ EdgeGrid.prototype.auth = function(req) {
     if (typeof(req.body) == 'object') {
       req.body = JSON.stringify(req.body);
     }
+    req.body = jsesc(req.body);
   }
 
   this.request = auth.generateAuth(
